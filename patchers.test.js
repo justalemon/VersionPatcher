930b434046ee4557df122048a46d942542652f19
Hannele Ruiz <justlemoncl@gmail.com>
2022-06-07 16:49:45 -0400

    Simplify the test for csproj patching

--- patchers.test.js
+++ patchers.test.js
@@ -14,11 +14,9 @@ describe("patchers", () => {
 
         await patchers.patchcsproj(path, "2.3.4");
 
-        const original = fs.readFileSync("files/csproj/TestProject.csproj", "utf-8");
         const patched = fs.readFileSync(path, "utf-8");
         const done = fs.readFileSync("files/csproj/TestProject.Ready.csproj", "utf-8");
 
-        expect(patched).not.toEqual(original);
         expect(patched).toEqual(done);
     });
 });
