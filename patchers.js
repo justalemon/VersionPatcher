f2c6a8bd1269dd896be06ae1e37f014ad9109efd
Hannele Ruiz <justlemoncl@gmail.com>
2022-06-07 13:59:08 -0400

    Added patching of csproj files

--- patchers.js
+++ patchers.js
@@ -1,24 +1,34 @@
+const core = require("@actions/core");
 const fs = require("fs");
 const glob = require("@actions/glob");
 const xml2js = require("xml2js");
-
-const parser = new xml2js.Parser({});
-const builder = new xml2js.Builder({});
 
 // *should* comply with PEP440
 const regex_setuppy = new RegExp("__version__ ?= ?[\"']v?(?:([0-9]+)!)?([0-9]+(?:\\.[0-9]+)*)([-_\\.]?(a|b|c|rc|alpha|beta|pre|preview)[-_\\.]?([0-9]+)?)?((?:-([0-9]+))[-_\\.]?(post|rev|r)[-_\\.]?([0-9]+)?)?([-_\\.]?(dev)[-_\\.]?([0-9]+)?)?(?:\\+([a-z0-9]+(?:[-_\\.][a-z0-9]+)*))?[\"']");
 
 exports.patchcsproj = async function (glob_str, version)
 {
-    const globber = await glob.create(glob_str);
-
-    for await (const file of globber.globGenerator())
+    for await (const file of (await glob.create(glob_str)).globGenerator())
     {
-        const contents = fs.readFileSync(file);
-        const parsed = parser.parseString(contents);
+        const contents = fs.readFileSync(file, "utf-8");
 
-        const xml = builder.buildObject(parsed);
-        fs.writeFileSync(file, xml);
+        new xml2js.Parser({}).parseString(contents, (err, result) => {
+            if (err)
+            {
+                core.setFailed(err);
+            }
+
+            const array = result["Project"]["PropertyGroup"];
+            array.forEach((value, _) => {
+                if (value.hasOwnProperty("Version"))
+                {
+                    value["Version"] = [version];
+                }
+            })
+
+            const xml = new xml2js.Builder({headless: true}).buildObject(result);
+            fs.writeFileSync(file, xml);
+        });
     }
 }
 
