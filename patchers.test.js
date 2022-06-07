700566acb162297913d3c4c1fc7ca56468fb79c2
Hannele Ruiz <justlemoncl@gmail.com>
2022-06-07 14:27:09 -0400

    Reworked csproj test

--- patchers.test.js
+++ patchers.test.js
@@ -3,9 +3,22 @@ const patchers = require("./patchers.js");
 
 describe("patchers", () => {
     test("Patch csproj", async () => {
-        await patchers.patchcsproj("files/TestProject.csproj", "2.3.4");
-        const patched = fs.readFileSync("files/TestProject.csproj", "utf-8");
-        const sample = fs.readFileSync("files/TestProject.Ready.csproj", "utf-8");
-        expect(patched).toEqual(sample);
+        const path = "files/csproj/TestProject.Edit.csproj";
+
+        if (fs.existsSync(path))
+        {
+            fs.unlinkSync(path);
+        }
+
+        fs.copyFileSync("files/csproj/TestProject.csproj", path);
+
+        await patchers.patchcsproj(path, "2.3.4");
+
+        const original = fs.readFileSync("files/csproj/TestProject.csproj", "utf-8");
+        const patched = fs.readFileSync(path, "utf-8");
+        const done = fs.readFileSync("files/csproj/TestProject.Ready.csproj", "utf-8");
+
+        expect(patched).not.toEqual(original);
+        expect(patched).toEqual(done);
     })
 })
