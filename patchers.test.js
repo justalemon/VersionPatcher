340e08459734f2322ac398a8edc549973fe1779c
Hannele Ruiz <justlemoncl@gmail.com>
2022-06-07 18:33:12 -0400

    Improved the test function for future tests

--- patchers.test.js
+++ patchers.test.js
@@ -1,16 +1,32 @@
 const fs = require("fs");
 const patchers = require("./patchers.js");
 
+async function match(from, to, ready, callback) {
+    if (fs.existsSync(to))
+    {
+        fs.unlinkSync(to);
+    }
+
+    fs.copyFileSync(from, to);
+
+    await callback();
+
+    const patched = fs.readFileSync(to, "utf-8");
+    const sample = fs.readFileSync(ready, "utf-8");
+
+    fs.unlinkSync(to);
+
+    expect(patched).toEqual(sample);
+}
+
 describe("patchers", () => {
     test("Patch csproj", async () => {
-        const path = "files/csproj/TestProject.Edit.csproj";
+        const from = "files/csproj/TestProject.csproj";
+        const to = "files/csproj/TestProject.Edit.csproj";
+        const ready = "files/csproj/TestProject.Ready.csproj";
 
-        if (fs.existsSync(path))
-        {
-            fs.unlinkSync(path);
-        }
-
-        fs.copyFileSync("files/csproj/TestProject.csproj", path);
+        await match(from, to, ready, async () => await patchers.patchcsproj(to, "2.3.4"))
+    });
 
         await patchers.patchcsproj(path, "2.3.4");
 
